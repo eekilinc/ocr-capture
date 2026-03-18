e7eb41937405d7135f4a0957ec92359bbcf1f48e
eekilinc <ekilinc@mehmetakif.edu.tr>
2026-03-19 01:31:05 +0300

    chore: sync all versions to v1.1.7

--- src/App.tsx
+++ src/App.tsx
@@ -712,7 +712,7 @@ function App() {
         onClose={() => setIsSettingsOpen(false)}
         theme={theme}
         onThemeChange={setTheme}
-        appVersion="1.1.6"
+        appVersion="1.1.7"
         currentShortcut={currentShortcut}
         onShortcutUpdate={(newShortcut) => setCurrentShortcut(newShortcut)}
         ocrLanguages={ocrLanguages}
